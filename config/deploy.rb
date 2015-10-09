b90ebb2a6c03672729d843660aba8457211f2ab6
waqar.baig <waqar.shahid@pixelcrayons.com>
2015-10-09 19:26:00 +0530

    default env in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
