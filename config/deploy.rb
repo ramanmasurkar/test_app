26d559f056ec6e0e9e4c87ecad128aba786946b8
waqar.baig <waqar.shahid@pixelcrayons.com>
2015-10-09 19:20:00 +0530

    use_sudo true in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,7 @@ set :deploy_to, '/var/www/test_app'
 set :repo_url, 'https://github.com/ramanmasurkar/test_app.git'
 # Default value for :scm is :git
 set :scm, :git
+set :use_sudo, true
 
 # Default value for :format is :pretty
 # set :format, :pretty
